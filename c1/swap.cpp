c60ffcc8fb94bb9425d838e51ce9b3da9fa4ebeb
kestrel <228611676@qq.com>
2020-01-04 09:13:54 -0500

    add a reverse array function

--- c1/swap.cpp
+++ c1/swap.cpp
@@ -6,13 +6,21 @@ void inplace_swap(int *x,int *y)
   *x = *x^*y;
 }
 
-
+void reverse_array(int *a,int l)
+{
+  
+  for(int first=0,last=l; first<last;first++,last--)
+  { 
+      inplace_swap(&a[first],&a[last]);
+  }
+}
 
 int main()
 {
-  int a1 =1;
-  int a2 =2;
-  inplace_swap(&a1,&a2);
-  printf("%d,%d\n",a1,a2);
+  int a[] = {1,2,3,4,5};
+
+  reverse_array(a,4);
+  for(int i =0;i<5;i++)
+    printf("%d ",a[i]);
   return 0;
 }
